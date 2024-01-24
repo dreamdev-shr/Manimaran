80c491dbd694f6a5dd8671babb9c1446414f79b8
ManimaranB1 <157122961+ManimaranB1@users.noreply.github.com>
2024-01-24 11:31:06 +0530

    migration
    migration creation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -167,10 +167,10 @@
   </div>
 </div>
 
-<hr class="featurette-divider m-0">
+<hr class="featurette-divider m-0 ">
 
 <div class="row featurette" style="background-color: rgb(217, 218, 235);">
-  <div class="col-md-5 order-md-1">
+  <div class="col-md-5 order-md-2 m-3">
       <img src="{{asset('imge/pexels-deane-bayas-11960288.jpg')}}" alt="" class="img object-fit-cover w-100 h-100">
   </div>
   <div class="col-md-7 order-md-2 text-center ">
@@ -181,76 +181,51 @@
 
 
 </div>
-<div class="card-group m-5">
-    <div class="card">
-      <img src="{{asset('imge/c1 (2).jpg')}}" alt="" class="img object-fit-cover w-100 h-100">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+<div class="b-example-divider" ></div>
+
+
+<div class="container">
+  <footer class="py-5">
+    <div class="row">
+
+
+
+
+      <div class="col-6 col-md-2 mb-3">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+        </ul>
       </div>
 
-      <div class="card-footer">
-        <small class="text-body-secondary">Last updated 3 mins ago</small>
+      <div class="col-md-5 offset-md-1 mb-3">
+        <form>
+          <h5>Subscribe to our newsletter</h5>
+          <p>Monthly digest of what's new and exciting from us.</p>
+          <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+            <label for="newsletter1" class="visually-hidden">Email address</label>
+            <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+            <button class="btn btn-primary" type="button">Subscribe</button>
+          </div>
+        </form>
       </div>
     </div>
-    <div class="card">
-      <div class="card-body">
-      <img src="{{asset('imge/c1 (2).jpg')}}" alt="" class="img object-fit-cover w-100 h-100">
 
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-body-secondary">Last updated 3 mins ago</small>
-      </div>
+    <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+      <p>&copy; 2023 Company, Inc. All rights reserved.</p>
+      <ul class="list-unstyled d-flex">
+        <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
+        <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
+        <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
+      </ul>
     </div>
-    <div class="card">
-      <div class="card-body">
-      <img src="{{asset('imge/c1 (2).jpg')}}" alt="" class="img object-fit-cover w-100 h-100">
-
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-body-secondary">Last updated 3 mins ago</small>
-      </div>
-    </div>
-  </div>
-  <div class="card-group">
-    <div class="card">
-      <img src="{{asset('imge/c1 (2).jpg')}}" alt="" class="img object-fit-cover w-100 h-100">
-
-      <div class="card-body">
-
-
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-body-secondary">Last updated 3 mins ago</small>
-      </div>
-    </div>
-    <div class="card">
-      <img src="{{asset('imge/c1 (2).jpg')}}" alt="" class="img object-fit-cover w-100 h-100">
-
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-body-secondary">Last updated 3 mins ago</small>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-body-secondary">Last updated 3 mins ago</small>
-      </div>
-    </div>
-  </div>
+  </footer>
+</div>
 
 </body>
 </html>
